fc209d60b0b47630d7f56f5ab02be1c814cd2f99
Suprav <supravchand2@gmail.com>
2026-09-05 14:58:30 +0545

    done

--- Backend/src/__init__.py
+++ Backend/src/__init__.py
@@ -28,7 +28,7 @@ app=FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["https://educate-yourself-1.onrender.com/"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
